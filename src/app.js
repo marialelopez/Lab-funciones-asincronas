d11e293d45a256b7d396f7f16019b2ad9e8b797b
Alejandra Lopez <mlopezalejandra13@gmail.com>
2023-09-13 20:05:43 -0500

    funciones asincronas

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
-function getRandomNumber() {}
+function getRandomNumber() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        const numeroAleatorio = Math.floor(Math.random() * 10); // Genera un número aleatorio entre 0 y 10
+        resolve(numeroAleatorio); // Resuelve la promesa con el número aleatorio
+      },3000);// Retraso de 3 segundos (3000 milisegundos)
+});}
+
+// Uso de la función getRandomNumber
+getRandomNumber()
+  .then((number) => {
+    console.log('Número aleatorio:', number);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 
 module.exports = getRandomNumber;
